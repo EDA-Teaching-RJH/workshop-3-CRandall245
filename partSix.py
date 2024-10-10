1a51f8d4bd02aa84bfd2315a4e8b5439c3804dd7
CRandall245 <ce327@kent.ac.uk>
2024-10-10 13:08:07 +0000

    commit 79?

--- partSix.py
+++ partSix.py
@@ -1,1 +1,13 @@
+def main():
+    age = input(int("How old are you?"))
+    stud = input("Are you a student? Y/N")
+    if age < 12 or age > 64:
+        print("That will cost £5")
+    elif stud == "Y":
+        print ("That will cost £8")
+    elif age > 12 and age < 65 and stud == "N":
+        print ("that will cost £10")
+    else:
+        print("error")
 
+main()
