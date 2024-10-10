d8b41b5d8e18872d3609a1fa8d3d492ad7034fc2
CRandall245 <ce327@kent.ac.uk>
2024-10-10 12:33:08 +0000

    commit2

--- partThree.py
+++ partThree.py
@@ -1,12 +1,12 @@
 def main():
     perc = int(input("What percentage did you score?"))
-    if 100 >= perc <= 90:
+    if 100 >= perc and perc <= 90:
         print("Grade: A") 
-    elif 89 >= perc <= 80:
+    elif 89 >= perc and perc <= 80:
         print("Grade: B")
-    elif 79 >= perc <= 70:
+    elif 79 >= perc and perc <= 70:
         print("Grade: C")
-    elif 69 >= perc <= 60:
+    elif 69 >= perc and perc <= 60:
         print("Grade: D")
     elif 59 >= perc:
         print("Grade: F")
