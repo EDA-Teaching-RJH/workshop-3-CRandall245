3c96175285f0fc6de2dbc9594f9bf245aae56cd6
CRandall245 <ce327@kent.ac.uk>
2024-10-10 12:57:55 +0000

    commit42?

--- partFive.py
+++ partFive.py
@@ -1,1 +1,10 @@
+def main():
+    week = input("What day of the week is it?")
+    if week == "Saturday" or week == "Sunday":
+        print("Its the weekend")
+    elif week == "Monday" or week == "Tuesday" or week == "Wednesday" or week == "Thursday" or week == "Friday":
+        print("Its a weekday")
+    else:
+        print("error")
 
+main()
