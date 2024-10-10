3359974d28b118702626aa504c62568385aa72d6
CRandall245 <ce327@kent.ac.uk>
2024-10-10 13:21:38 +0000

    commit 404?

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,23 @@
+def main():
+    loop = True
+    while loop == True:
+        num1 = int(input("Give me a number:"))
+        num2 = int(input("Give me a number:"))
+        op = input("Give me an operator or type quit:")
+        match op:
+            case "+":
+                print("The answer is " , (num1 + num2))
+            case "-":
+                print("The answer is " , (num1 - num2))
+            case "/":
+                print("The answer is " , (num1 / num2))
+            case "*":
+                print("The answer is " , (num1 * num2))
+            case "^":
+                print("The answer is " , (num1 ** num2))
+            case "%":
+                print("The answer is " , (num1 % num2))
+            case "quit":
+                loop = False
 
+main()
