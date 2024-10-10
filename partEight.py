67918375cb0c5268f74df73630de544d3ad7ef50
CRandall245 <ce327@kent.ac.uk>
2024-10-10 13:50:32 +0000

    commit 743

--- partEight.py
+++ partEight.py
@@ -1,1 +1,21 @@
+def dec1():
+    choice = input("There is an entrance in front of you, do you enter? Y/N")
+    if choice == "Y":
+        dec2()
+    elif choice == "N":
+        print("You reject the call of adventure")
+    else:
+        print("error")
 
+def dec2():
+    choice = input("There is a split in the pathon the left and right. Where do you go? L/R")
+    if choice == "L":
+        dec3()
+    elif
+        print("you fall in a pitfall trap and die to punji sticks")
+    else:
+        print("error")
+
+def dec3():
+    choice = input("")
+dec1()
