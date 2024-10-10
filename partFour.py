efd6a64620854af232f122ad04b7bfd12b7842d5
CRandall245 <ce327@kent.ac.uk>
2024-10-10 12:52:36 +0000

    commmit4?

--- partFour.py
+++ partFour.py
@@ -1,1 +1,20 @@
+def main():
+    InpUser = input("Username:")
+    InpPass = input("Password:")
+    if InpUser == "robert" and InpPass == "amazingDude":
+        print("access denied")
+    else:
+        match InpUser:
+            case "admin":
+                print("known user")
+            case _:
+                print("unknown user")
+        match InpPass:
+            case "password":
+                print("Correct Password")
+            case _:
+                print("Incorrect Password")
+
+main()
+
 
