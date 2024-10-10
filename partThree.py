55b6a83a36f8fb5b9579503d7d0517e38e219cd8
CRandall245 <ce327@kent.ac.uk>
2024-10-10 12:31:51 +0000

    commit 1

--- partThree.py
+++ partThree.py
@@ -1,1 +1,16 @@
+def main():
+    perc = int(input("What percentage did you score?"))
+    if 100 >= perc <= 90:
+        print("Grade: A") 
+    elif 89 >= perc <= 80:
+        print("Grade: B")
+    elif 79 >= perc <= 70:
+        print("Grade: C")
+    elif 69 >= perc <= 60:
+        print("Grade: D")
+    elif 59 >= perc:
+        print("Grade: F")
+    else:
+        print("Error")
 
+main()
--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,16 @@
+import random
 
+def main():
+    obj = random.randint(1, 10)
+    loop = True
+    while loop == True:
+        user_inp = int(input("Guess a number:"))
+        if user_inp < obj:
+            print("too low")
+        elif user_inp > obj:
+            print("too high")
+        else:
+            print("correct")
+            loop = False
+
+main()
